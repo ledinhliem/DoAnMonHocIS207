2ac3b2056ca82f313c6202b3b4dda45bbd89d1a1
phucnt-uit <nguyphuc240806@gmail.com>
2026-05-26 12:30:25 +0700

    Phuc cap nhat thanh toan Live API - Chuyen tu Sandbox sang Production

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/core/Database.php';
 require_once __DIR__ . '/core/Model.php';
